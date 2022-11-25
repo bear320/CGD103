572783d183a2d61e270f8b573a07f663826377da
bear320 <aaabear320@gmail.com>
2022-11-25 10:10:58 +0800

    updated

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+import gsap from "gsap";
+
+console.log("start");
+
+// jquery
+$("body").css("background-color", "#333");
+
+// gsap
+gsap.set(".box", {
+    x: 300,
+    y: 300,
+});
+gsap.to(".box", {
+    duration: 2,
+    rotation: 360,
+    repeat: -1,
+    scale: 3,
+});
